7078d4659cccef0bee56981edfe556299ec8901c
Artenis Molla <artenismolla@MacBook-Pro-di-Artenis.local>
2022-06-03 00:27:17 +0200

    Planner UI miglioramenti

--- lib/app/plantrip/widget/current_planned_trip_widget.dart
+++ lib/app/plantrip/widget/current_planned_trip_widget.dart
@@ -82,12 +82,63 @@ class CurrentPlannedTripList extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        CircleAvatar(
-            radius: 50,
-            backgroundColor: UIColors.bluelight,
-            child: Stack(
-              children: [
-                ClipRRect(
+        Padding(
+          padding: const EdgeInsets.all(0),
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Color(0xff9fccfa), //f0ebc0
+                    Color(0xff0974f1), //9dddf4
+                    //e93a28
+                  ]),
+                  shape: BoxShape.circle),
+              child: Padding(
+                padding: const EdgeInsets.all(2),
+                child: CircleAvatar(
+                  radius: 53,
+                  backgroundColor: Colors.white,
+                  child: CircleAvatar(
+                      radius: 50,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: CachedNetworkImage(
+                          imageUrl:
+                              'https://images.unsplash.com/photo-1528744598421-b7b93e12df15?ixlib=rb-1.2.1&ixid=&auto=format&fit=crop&w=928&q=80',
+                          imageBuilder: (context, imageProvider) => Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: imageProvider, fit: BoxFit.cover),
+                            ),
+                          ),
+                          placeholder: (context, url) =>
+                              const LoadingIndicator(),
+                          errorWidget: (context, url, error) => Center(
+                            child: Icon(Iconsax.gallery_slash,
+                                size: 30, color: UIColors.lightRed),
+                          ),
+                        ),
+                      )),
+                ),
+              ),
+            ),
+          ),
+        ),
+        /*Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color(0xff84ffc9), //f0ebc0
+                Color(0xffaab2ff), //9dddf4
+                Color(0xffeca0ff), //e93a28
+              ]),
+              shape: BoxShape.circle),
+          child: Padding(
+            padding: const EdgeInsets.all(2.5),
+            child: CircleAvatar(
+                radius: 50,
+                backgroundColor: UIColors.bluelight,
+                child: ClipRRect(
                   borderRadius: BorderRadius.circular(50),
                   child: CachedNetworkImage(
                     imageUrl:
@@ -104,34 +155,23 @@ class CurrentPlannedTripList extends StatelessWidget {
                           size: 30, color: UIColors.lightRed),
                     ),
                   ),
-                ),
-                Positioned.fill(
-                    child: Align(
-                        alignment: Alignment.center,
-                        child: Container(
-                          padding: const EdgeInsets.only(
-                              top: 8, bottom: 8, left: 8, right: 8),
-                          decoration: BoxDecoration(
-                              color: UIColors.white.withOpacity(0.6),
-                              borderRadius: BorderRadius.circular(20)),
-                          child: const Icon(
-                            Iconsax.play,
-                            size: 25,
-                            color: Colors.black,
-                          ),
-                        )))
-              ],
-            )),
+                )),
+          ),
+        ),*/
         const SizedBox(height: 10),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              pTrip.tripName ?? '',
-              style: GoogleFonts.poppins(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black),
+            Expanded(
+              child: Text(
+                pTrip.tripName ?? '',
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black),
+              ),
             ),
           ],
         ),
