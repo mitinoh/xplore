9eb5c01a19e12d20378d80ca53c8468308f308b9
Artenis Molla <artenismolla@MacBook-Pro-di-Artenis.local>
2022-07-17 22:37:39 +0200

    Logica Reports, manca solo la connessione con il db

--- lib/core/widgets/navbar.dart
+++ lib/core/widgets/navbar.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
@@ -6,10 +5,8 @@ import 'package:xplore/app/add_location/screen/new_location_screen.dart';
 import 'package:xplore/app/home/screen/home_screen.dart';
 import 'package:xplore/app/map/screen/map_screen.dart';
 import 'package:xplore/app/plantrip/screen/plan_trip_screen.dart';
-import 'package:xplore/app/user/bloc_saved_location/saved_location_bloc.dart';
 import 'package:xplore/app/user/screen/dashboard.dart';
 import 'package:xplore/core/UIColors.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Navbar extends StatelessWidget {
   Navbar({Key? key}) : super(key: key);
--- lib/core/widgets/report_bottom_sheet.dart
+++ lib/core/widgets/report_bottom_sheet.dart
@@ -1,13 +1,31 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:xplore/core/UIColors.dart';
 
 // ignore: must_be_immutable
-class ReportBottomSheet extends StatelessWidget {
+class ReportBottomSheet extends StatefulWidget {
   const ReportBottomSheet({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<ReportBottomSheet> createState() => _ReportBottomSheetState();
+}
+
+class _ReportBottomSheetState extends State<ReportBottomSheet> {
+  late int _pageReportIndex = 0;
+  late int _reportTypeIndex = 0;
+  final List<String> _reportType = [
+    "Spam",
+    "Nudo o atti sessuali",
+    "Truffa o frode",
+    "Discorsi o simboli che incitano all'odio",
+    "Bullismo o intimidazioni",
+    "Altro"
+  ];
   @override
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context);
@@ -25,6 +43,24 @@ class ReportBottomSheet extends StatelessWidget {
       child: SafeArea(
         child: SingleChildScrollView(
           child: Column(
+            children: [
+              firstStep(lightDark),
+              secondStep(lightDark),
+              thirdStep(lightDark),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Visibility firstStep(ThemeData lightDark) {
+    return Visibility(
+      visible: _pageReportIndex == 0 ? true : false,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
@@ -35,142 +71,183 @@ class ReportBottomSheet extends StatelessWidget {
                     fontWeight: FontWeight.w600,
                     color: lightDark.primaryColor),
               ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0, right: 20),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                          "Segnala questa account allo staff di XPLORE, nel caso in qui questo utente non rispetta le norme della community.",
-                          overflow: TextOverflow.visible,
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.poppins(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w300,
-                              color: Colors.grey)),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                padding: const EdgeInsets.only(top: 20, bottom: 20),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: UIColors.platinium),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Spam",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.poppins(
-                              fontSize: 14.5,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black),
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      height: 30,
-                      thickness: 2,
-                      color: lightDark.scaffoldBackgroundColor,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Nudo o atti sessuali",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.poppins(
-                              fontSize: 14.5,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black),
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      height: 30,
-                      thickness: 2,
-                      color: lightDark.scaffoldBackgroundColor,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Truffa o frode",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.poppins(
-                              fontSize: 14.5,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black),
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      height: 30,
-                      thickness: 2,
-                      color: lightDark.scaffoldBackgroundColor,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Discorsi o simboli che incitano all'odio",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.poppins(
-                              fontSize: 14.5,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black),
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      height: 30,
-                      thickness: 2,
-                      color: lightDark.scaffoldBackgroundColor,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Bullismo o intimidazioni",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.poppins(
-                              fontSize: 14.5,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black),
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      height: 30,
-                      thickness: 2,
-                      color: lightDark.scaffoldBackgroundColor,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Altro...",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.poppins(
-                              fontSize: 14.5,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )
             ],
           ),
-        ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                      "Segnala questa account allo staff di XPLORE, nel caso in qui questo utente non rispetta le norme della community.",
+                      overflow: TextOverflow.visible,
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.grey)),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.only(top: 20, bottom: 20),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: UIColors.platinium),
+            child: Column(
+              children: [
+                ListView.separated(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  separatorBuilder: (_, __) => Divider(
+                    height: 30,
+                    thickness: 2,
+                    color: lightDark.scaffoldBackgroundColor,
+                  ),
+                  itemCount: _reportType.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      onTap: () {
+                        setState(() {
+                          _pageReportIndex = 1;
+                          _reportTypeIndex = index;
+                        });
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            _reportType[index].toString(),
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.poppins(
+                                fontSize: 14.5,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  Visibility thirdStep(ThemeData lightDark) {
+    return Visibility(
+        visible: _pageReportIndex == 2 ? true : false,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Segnalazione effettuata",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: lightDark.primaryColor),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                        "Ti ringraziamo per aver contribuito a rendere XPLORE un posto migliore. :)",
+                        overflow: TextOverflow.visible,
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.grey)),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Visibility secondStep(ThemeData lightDark) {
+    return Visibility(
+        visible: _pageReportIndex == 1 ? true : false,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                    onTap: () {
+                      setState(() {
+                        _pageReportIndex = 0;
+                      });
+                    },
+                    child: const Icon(Iconsax.arrow_left)),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _pageReportIndex = 2;
+                    });
+                  },
+                  child: Text("invia",
+                      style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: UIColors.lightblue)),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                        "Secondo passo. Stai segnalato questo utente per la seguente voce:",
+                        overflow: TextOverflow.visible,
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.grey)),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  _reportType[_reportTypeIndex].toString(),
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: lightDark.primaryColor),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
