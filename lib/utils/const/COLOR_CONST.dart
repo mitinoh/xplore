edd2d1b06f7025b52c3329ea8a525f2cce3b0e27
mitinoh <mite.gj9@gmail.com>
2022-10-17 23:25:41 +0200

    Rimossi commenti

--- lib/utils/const/COLOR_CONST.dart
+++ lib/utils/const/COLOR_CONST.dart
@@ -24,47 +24,4 @@ class COLOR_CONST {
 
   static final HexColor GREY = new HexColor("#FF161616");
   static final HexColor DIRTYWHITE = new HexColor("#FFF3F7FA");
-  // static final HexColor DEFAULT = new HexColor("#E81667");
-  // static final HexColor DEFAULT_5 = new HexColor("#0DE81667");
-  // static final HexColor RED = new HexColor("#F93F3E");
-  // static final HexColor RED2 = new HexColor("#D05045");
-  // static final HexColor GREEN = new HexColor("#2EC492");
-  // static final HexColor GREEN2 = new HexColor("#8CC153");
-  // static final HexColor ORANGE = new HexColor("#EB8D2F");
-  // static final HexColor WHITE = new HexColor("#FFFFFF");
-  // static final HexColor WHITE_50 = new HexColor("#80FFFFFF");
-  // static final HexColor WHITE_70 = new HexColor("#B3FFFFFF");
-  // static final HexColor BLACK = new HexColor("#000000");
-  // static final HexColor BLACK2 = new HexColor("#333333");
-  // static final HexColor BLACK_30 = new HexColor("#4D000000");
-  // static final HexColor GRAY1 = new HexColor("#999999");
-  // static final HexColor GRAY1_50 = new HexColor("#80999999");
-  // static final HexColor GRAY1_70 = new HexColor("#B3999999");
-  // static final HexColor GRAY2 = new HexColor("#F8F8F8");
-  // static final HexColor GRAY3 = new HexColor("#F4F4F4");
-  // static final HexColor GRAY4 = new HexColor("#666666");
-  // static final HexColor GRAY4_40 = new HexColor("#66666666");
-  // static final HexColor GRAY5 = new HexColor("#C1C1C1");
-  // static final HexColor GRAY6 = new HexColor("#707070");
-  // static final HexColor GRAY7 = new HexColor("#DDDDDD");
-  // static final HexColor ITEM_BG = new HexColor("#F3F6F8");
-  // static final HexColor BLUE = new HexColor("#222539");
-  // static final HexColor DIVIDER = new HexColor("#33000000");
-  // static final HexColor TRANS = new HexColor("00000000");
-  // static final HexColor DEFAULT_BORDER = new HexColor("#E5E5E5");
-//
-  // static final HexColor GOOGLE_BTN = new HexColor("#FFF1F0");
-  // static final HexColor GOOGLE_BORDER_BTN = new HexColor("#F14336");
-  // static final HexColor FACEBOOK_BTN = new HexColor("#F5F9FF");
-  // static final HexColor FACEBOOK_BORDER_BTN = new HexColor("#3164CE");
-  // static final HexColor TAG_BG = new HexColor("#FFE5EF");
-  // static final HexColor GIFT_BG1 = new HexColor("#FCCAC6");
-  // static final HexColor GIFT_BG2 = new HexColor("#2687D57E");
-  // static final HexColor TIME_SLOT_BG = new HexColor("#FCFCFC");
-  // static final HexColor TIME_SLOT_BORDER = new HexColor("#E5E5E5");
-  // static final HexColor SEAT_SLOT_BG_BOOKED = new HexColor("#EBEBEB");
-  // static final HexColor SEAT_SLOT_BG = new HexColor("#FFFFFF");
-  // static final HexColor SEAT_SLOT_BORDER = new HexColor("#707070");
-//
-  // static final MaterialColor STATUS_BAR = MaterialColor(0xFF2E3147, {});
 }
