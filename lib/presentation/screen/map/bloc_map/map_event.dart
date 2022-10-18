bc06b204fff8ea8aed4e63aa56b445622328972e
mitinoh <mite.gj9@gmail.com>
2022-10-18 21:46:10 +0200

    Rimossi import

--- lib/presentation/screen/map/bloc_map/map_event.dart
+++ lib/presentation/screen/map/bloc_map/map_event.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:equatable/equatable.dart';
-import 'package:xplore/data/api/mongoose.dart';
-import 'package:xplore/data/model/location_model.dart';
-import 'package:xplore/data/api/mongoose.dart';
-import 'package:xplore/data/api/rest_client.dart';
-import 'package:xplore/data/dio_provider.dart';
-import 'package:xplore/data/model/location_model.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:xplore/data/api/mongoose.dart';
-import 'package:xplore/data/model/location_model.dart';
-import 'package:xplore/presentation/common_widgets/detail_location_modal.dart';
-import 'package:xplore/presentation/screen/map/bloc_map/bloc.dart';
-import 'package:xplore/presentation/screen/map/widget/wg_marker.dart';
 
 @immutable
 abstract class MapEvent extends Equatable {
