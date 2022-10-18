5e24f25f5d71d1bb11e9fb733131bd6fad9ef0ff
mitinoh <mite.gj9@gmail.com>
2022-10-18 22:39:03 +0200

    Snackbar error

---
+++ lib/presentation/common_widgets/sb_error.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SbError extends StatelessWidget {
+  const SbError({Key? key, this.message = 'Ops something went wrong...'})
+      : super(key: key);
+  final String message;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  show(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      content: _build(context),
+    ));
+  }
+
+  Widget _build(BuildContext context) {
+    ThemeData themex = Theme.of(context);
+    MediaQueryData mediaQueryX = MediaQuery.of(context);
+
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          height: 90,
+          decoration: BoxDecoration(
+              color: themex.backgroundColor,
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+          child: Row(
+            children: [
+              const SizedBox(width: 48),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      message,
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w400,
+                          color: themex.indicatorColor,
+                          fontSize: 14),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+            top: -15,
+            left: -10,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(20)),
+              child: Stack(
+                children: [
+                  Image.asset(
+                    "assets/map.png",
+                    scale: 5,
+                  ),
+                ],
+              ),
+            ))
+      ],
+    );
+  }
+}
